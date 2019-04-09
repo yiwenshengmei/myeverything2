fb2e86f61d52199f1f2848de2965e02ecba55f6b
zhaojie <yiwenshengmei@163.com>
2019-04-09 15:23:12 +0800

    1

---
+++ MyEverythingDB.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MyEverything {
+	public enum MyEverythingRecordType {
+		File, Folder
+	}
+	
+	public class MyEverythingDB {
+		private Dictionary<string, Dictionary<ulong, MyEverythingRecord>> _volumes_files = new Dictionary<string, Dictionary<ulong, MyEverythingRecord>>();
+		private Dictionary<string, Dictionary<ulong, MyEverythingRecord>> _volumes_folders = new Dictionary<string, Dictionary<ulong, MyEverythingRecord>>();
+
+		public MyEverythingDB() { }
+
+		public bool ContainsVolume(string volume) {
+			return _volumes_files.ContainsKey(volume) && _volumes_folders.ContainsKey(volume);
+		}
+		public void AddRecord(string volume, List<MyEverythingRecord> r, MyEverythingRecordType type) {
+			if (type == MyEverythingRecordType.File) {
+				CheckHashTableKey(_volumes_files, volume);
+				r.ForEach(x => _volumes_files[volume].Add(x.FRN, x));
+			} else {
+				CheckHashTableKey(_volumes_folders, volume);
+				r.ForEach(x => _volumes_folders[volume].Add(x.FRN, x));
+			}
+		}
+		public void AddRecord(string volume, MyEverythingRecord record, MyEverythingRecordType type) {
+			if (type == MyEverythingRecordType.File) {
+				CheckHashTableKey(_volumes_files, volume);
+				_volumes_files[volume].Add(record.FRN, record);
+			} else {
+				CheckHashTableKey(_volumes_folders, volume);
+				_volumes_folders[volume].Add(record.FRN, record);
+			}
+		}
+		private void CheckHashTableKey(Dictionary<string, Dictionary<ulong, MyEverythingRecord>> hashtable, string key) {
+			if (!hashtable.ContainsKey(key)) 
+				hashtable.Add(key, new Dictionary<ulong,MyEverythingRecord>());
+		}
+		public bool DeleteRecord(string volume, ulong frn) {
+			bool result = false;
+			result = DeleteRecordHashTableItem(_volumes_files, volume, frn);
+			if (!result) result = DeleteRecordHashTableItem(_volumes_folders, volume, frn);
+			return result;
+		}
+		private bool DeleteRecordHashTableItem(Dictionary<string, Dictionary<ulong, MyEverythingRecord>> hashtable, string volume, ulong frn) {
+			if (hashtable.ContainsKey(volume) && hashtable[volume].ContainsKey(frn)) {
+				hashtable[volume].Remove(frn);
+				return true;
+			} else {
+				return false;
+			}
+		}
+		public void UpdateRecord(string volume, MyEverythingRecord record, MyEverythingRecordType type) {
+			if (type == MyEverythingRecordType.File)
+				RealUpdateRecord(volume, _volumes_files, record);
+			else
+				RealUpdateRecord(volume, _volumes_folders, record);
+		}
+		private bool RealUpdateRecord(string volume, Dictionary<string, Dictionary<ulong, MyEverythingRecord>> source, MyEverythingRecord record) {
+			if (source.ContainsKey(volume) && source[volume].ContainsKey(record.FRN)) {
+				source[volume][record.FRN] = record;
+				return true;
+			} else {
+				return false;
+			}
+		}
+		public List<MyEverythingRecord> FindByName(string filename, out long foundFileCnt, out long fountFolderCnt) {
+
+			var fileQuery = from filesInVolumeDic in _volumes_files.Values
+							from eachFilePair in filesInVolumeDic
+							where eachFilePair.Value.Name.Contains(filename)
+							select eachFilePair.Value;
+
+			var folderQuery = from fldsInVolumeDic in _volumes_folders.Values
+							  from eachFldPair in fldsInVolumeDic
+						      where eachFldPair.Value.Name.Contains(filename)
+						      select eachFldPair.Value;
+
+			foundFileCnt = fileQuery.Count();
+			fountFolderCnt = folderQuery.Count();
+
+			List<MyEverythingRecord> result = new List<MyEverythingRecord>();
+
+			result.AddRange(fileQuery);
+			result.AddRange(folderQuery);
+
+			return result;
+		}
+		public MyEverythingRecord FindByFrn(string volume, ulong frn) {
+			if ((!_volumes_files.ContainsKey(volume)) || (!_volumes_folders.ContainsKey(volume)))
+				throw new Exception(string.Format("DB not contain the volume: {0}", volume));
+			MyEverythingRecord result = null;
+			_volumes_files[volume].TryGetValue(frn, out result);
+			if (result != null) return result;
+			_volumes_folders[volume].TryGetValue(frn, out result);
+			return result;
+		}
+		public long FileCount {
+			get { return _volumes_files.Sum(x => x.Value.Count); }
+		}
+		public long FolderCount {
+			get { return _volumes_folders.Sum(x => x.Value.Count); }
+		}
+		public Dictionary<ulong, MyEverythingRecord> GetFolderSource(string volume) {
+			Dictionary<ulong, MyEverythingRecord> result = null;
+			_volumes_folders.TryGetValue(volume, out result);
+			return result;
+		}
+	}
+}
---
+++ VolumeMonitor.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace MyEverything {
+	class VolumeMonitor {
+
+		public Action<MyEverythingRecord> RecordAddedEvent;
+		public Action<MyEverythingRecord> RecordDeletedEvent;
+		public Action<MyEverythingRecord, MyEverythingRecord> RecordRenameEvent;
+
+		public void Monitor(List<string> volumes, MyEverythingDB db) {
+			foreach (var volume in volumes) {
+				if (string.IsNullOrEmpty(volume)) throw new InvalidOperationException("Volume cant't be null or empty string.");
+				if (!db.ContainsVolume(volume)) throw new InvalidOperationException(string.Format("Volume {0} must be scaned first."));
+				Thread th = new Thread(new ParameterizedThreadStart(MonitorThread));
+				th.Start(new Dictionary<string, object> { { "Volume", volume }, { "MyEverythingDB", db } });
+			}
+		}
+		private PInvokeWin32.READ_USN_JOURNAL_DATA SetupInputData4JournalRead(string volume, uint reason) {
+			IntPtr pMonitorVolume = MyEverything.GetVolumeJournalHandle(volume);
+			uint bytesReturned = 0;
+			PInvokeWin32.USN_JOURNAL_DATA ujd = new PInvokeWin32.USN_JOURNAL_DATA();
+			MyEverything.QueryUSNJournal(pMonitorVolume, out ujd, out bytesReturned);
+
+			// 构建输入参数
+			PInvokeWin32.READ_USN_JOURNAL_DATA rujd = new PInvokeWin32.READ_USN_JOURNAL_DATA();
+			rujd.StartUsn          = ujd.NextUsn;
+			rujd.ReasonMask        = reason;
+			rujd.ReturnOnlyOnClose = 1;
+			rujd.Timeout           = 0;
+			rujd.BytesToWaitFor    = 1;
+			rujd.UsnJournalID      = ujd.UsnJournalID;
+
+			return rujd;
+		}
+		private void MonitorThread(object param) {
+
+			MyEverythingDB db = (param as Dictionary<string, object>)["MyEverythingDB"] as MyEverythingDB;
+			string volume = (param as Dictionary<string, object>)["Volume"] as string;
+			IntPtr pbuffer = Marshal.AllocHGlobal(0x1000); // 构建输出参数
+			PInvokeWin32.READ_USN_JOURNAL_DATA rujd = SetupInputData4JournalRead(volume, 0xFFFFFFFF); // 对所有类型的reason都监听
+			UInt32 cbRead; // 用来存储实际输出的字节数
+			IntPtr prujd; // 指向输入参数结构体的指针
+
+			while (true) {
+
+				// 构建输入参数的指针
+				prujd = Marshal.AllocHGlobal(Marshal.SizeOf(rujd));
+				PInvokeWin32.ZeroMemory(prujd, Marshal.SizeOf(rujd));
+				Marshal.StructureToPtr(rujd, prujd, true);
+
+				Debug.WriteLine(string.Format("\nMoniting on {0}......", volume));
+				IntPtr pVolume = MyEverything.GetVolumeJournalHandle(volume);
+
+				bool fok = PInvokeWin32.DeviceIoControl(pVolume,
+					PInvokeWin32.FSCTL_READ_USN_JOURNAL,
+					prujd, Marshal.SizeOf(typeof(PInvokeWin32.READ_USN_JOURNAL_DATA)),
+					pbuffer, 0x1000, out cbRead, IntPtr.Zero);
+
+				IntPtr pRealData = new IntPtr(pbuffer.ToInt32() + Marshal.SizeOf(typeof(Int64))); // 返回的内存块头上的8个字节是一个usn_id, 从第9个字节开始才是record.
+				uint offset = 0;
+
+				if (fok) {
+					while (offset + Marshal.SizeOf(typeof(Int64)) < cbRead) { // record可能有多个!
+						PInvokeWin32.USN_RECORD usn = new PInvokeWin32.USN_RECORD(new IntPtr(pRealData.ToInt32() + (int) offset));
+						ProcessUSN(usn, volume, db);
+						offset += usn.RecordLength;
+					}
+				}
+
+				Marshal.FreeHGlobal(prujd);
+				rujd.StartUsn = Marshal.ReadInt64(pbuffer); // 返回的内存块头上的8个字节就是用来在进行下一次查询的
+			}
+		}
+		private void ProcessUSN(PInvokeWin32.USN_RECORD usn, string volume, MyEverythingDB db) {
+			var dbCached = db.FindByFrn(volume, usn.FRN);
+			MyEverything.FillPath(volume, dbCached, db);
+			Debug.WriteLine(string.Format("------USN[frn={0}]------", usn.FRN));
+			Debug.WriteLine(string.Format("FileName={0}, Reason={1}", usn.FileName, Reason.ReasonPrettyFormat(usn.Reason)));
+			Debug.WriteLine(string.Format("FileName[Cached]={0}", dbCached == null ? "NoCache": dbCached.FullPath));
+			Debug.WriteLine("--------------------------------------");
+
+			if (Util.MaskEqual(usn.Reason, Reason.USN_REASONS["USN_REASON_RENAME_NEW_NAME"]))
+				ProcessRenameNewName(usn, volume, db);
+			if ((usn.Reason & Reason.USN_REASONS["USN_REASON_FILE_CREATE"]) != 0)
+				ProcessFileCreate(usn, volume, db);
+			if (Util.MaskEqual(usn.Reason, Reason.USN_REASONS["USN_REASON_FILE_DELETE"]))
+				ProcessFileDelete(usn, volume, db);
+		}
+		private void ProcessFileDelete(PInvokeWin32.USN_RECORD usn, string volume, MyEverythingDB db) {
+			var cached = db.FindByFrn(volume, usn.FRN);
+			if (cached == null) {
+				return;
+			} else {
+				MyEverything.FillPath(volume, cached, db);
+				var deleteok = db.DeleteRecord(volume, usn.FRN);
+				Debug.WriteLine(string.Format(">>>> File {0} deleted {1}.", cached.FullPath, deleteok ? "successful" : "fail"));
+				if (RecordDeletedEvent != null)
+					RecordDeletedEvent(cached);
+			}
+		}
+		private void ProcessRenameNewName(PInvokeWin32.USN_RECORD usn, string volume, MyEverythingDB db) { 
+			MyEverythingRecord newRecord = MyEverythingRecord.ParseUSN(volume, usn);
+			//string fullpath = newRecord.Name;
+			//db.FindRecordPath(newRecord, ref fullpath, db.GetFolderSource(volume));
+			//newRecord.FullPath = fullpath;
+			var oldRecord = db.FindByFrn(volume, usn.FRN);
+			MyEverything.FillPath(volume, oldRecord, db);
+			MyEverything.FillPath(volume, newRecord, db);
+			Debug.WriteLine(string.Format(">>>> RenameFile {0} to {1}", oldRecord.FullPath, newRecord.FullPath));
+			db.UpdateRecord(volume, newRecord, 
+				usn.IsFolder ? MyEverythingRecordType.Folder : MyEverythingRecordType.File);
+			if (RecordRenameEvent != null) RecordRenameEvent(oldRecord, newRecord);
+			if (newRecord.FullPath.Contains("$RECYCLE.BIN")) {
+				Debug.WriteLine(string.Format(">>>> Means {0} moved to recycle.", oldRecord.FullPath));
+			}
+		}
+		private void ProcessFileCreate(PInvokeWin32.USN_RECORD usn, string volume, MyEverythingDB db) {
+			MyEverythingRecord record = MyEverythingRecord.ParseUSN(volume, usn);
+			//string fullpath = record.Name;
+			//db.FindRecordPath(record, ref fullpath, db.GetFolderSource(volume));
+			//record.FullPath = fullpath;
+			db.AddRecord(volume, record, usn.IsFolder ? MyEverythingRecordType.Folder : MyEverythingRecordType.File);
+			MyEverything.FillPath(volume, record, db);
+			Debug.WriteLine(string.Format(">>>> NewFile: {0}", record.FullPath));
+			if (RecordAddedEvent != null)
+				RecordAddedEvent(record);
+		}
+	}
+}
